51450e6ec6bbfbc621af38ec3bb45a4784cf610f
akinbezatoglu <akinbezatoglu@gmail.com>
2026-08-19 15:02:09 +0300

    fix(calloc): add integer overflow check for size calculation

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: abezatog <abezatog@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/12 18:10:21 by abezatog          #+#    #+#             */
-/*   Updated: 2026/08/15 00:23:08 by abezatog         ###   ########.fr       */
+/*   Updated: 2026/08/19 14:42:37 by abezatog         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,14 @@
 void	*ft_calloc(size_t nmemb, size_t size)
 {
 	void	*ptr;
+	size_t	total_size;
 
-	ptr = (void *)malloc(nmemb * size);
+	if (size != 0 && nmemb > ((size_t) - 1) / size)
+		return (NULL);
+	total_size = nmemb * size;
+	ptr = (void *)malloc(total_size);
 	if (!ptr)
 		return (NULL);
-	ft_bzero(ptr, nmemb * size);
+	ft_bzero(ptr, total_size);
 	return (ptr);
 }
