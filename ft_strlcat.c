80bb21f5cbacb3351a9906e429d57583de3f5a5a
akinbezatoglu <akinbezatoglu@gmail.com>
2026-08-20 18:15:07 +0300

    fix(strlcat): proper way to count dst len if dst is not null-terminated

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: abezatog <abezatog@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/05 21:48:05 by abezatog          #+#    #+#             */
-/*   Updated: 2026/08/10 22:33:54 by abezatog         ###   ########.fr       */
+/*   Updated: 2026/08/20 18:10:39 by abezatog         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,21 @@
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
 	size_t	i;
-	size_t	dstlen;
+	size_t	dst_len;
+	size_t	src_len;
 
-	dstlen = ft_strlen(dst);
-	if (size <= dstlen)
-		return (size + ft_strlen(src));
+	dst_len = 0;
+	while (dst_len < size && dst[dst_len])
+		dst_len++;
+	src_len = ft_strlen(src);
+	if (size <= dst_len)
+		return (src_len + size);
 	i = 0;
-	while (src[i] != '\0' && (dstlen + i) < (size - 1))
+	while (src[i] && i + dst_len + 1 < size)
 	{
-		dst[dstlen + i] = src[i];
+		dst[i + dst_len] = src[i];
 		i++;
 	}
-	dst[dstlen + i] = '\0';
-	return (dstlen + ft_strlen(src));
+	dst[dst_len + i] = '\0';
+	return (dst_len + src_len);
 }
