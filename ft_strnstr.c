a8be14d5125ee0b38cdfdbb45ff6aed346df20b4
akinbezatoglu <akinbezatoglu@gmail.com>
2026-08-21 12:46:33 +0300

    fix(strnstr): use strncmp to find substring

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -12,38 +12,16 @@
 
 #include "libft.h"
 
-static int	isfound(const char *big, const char *little, size_t len)
-{
-	while (len > 0 && *big && *little)
-	{
-		if (*big != *little)
-		{
-			return (0);
-		}
-		big++;
-		little++;
-		len--;
-	}
-	if (*little == '\0')
-		return (1);
-	return (0);
-}
-
 char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
-	size_t	l;
-
 	if (*little == '\0')
 		return ((char *)big);
-	if (len == 0)
-		return (NULL);
-	l = 0;
-	while (l < len && *big)
+	while (len > 0 && *big)
 	{
-		if (isfound(big, little, len - l))
-			return ((char *)(big));
+		if (ft_strncmp(big, little, len) == 0)
+			return ((char *)big);
 		big++;
-		l++;
+		len--;
 	}
 	return (NULL);
 }
