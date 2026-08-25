220a031c976626a47b2cd0a2cc7c35f4996b851e
akinbezatoglu <akinbezatoglu@gmail.com>
2026-08-25 13:56:54 +0300

    fix(strstr): reduce the search length until it equals the length of little

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: abezatog <abezatog@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/05 21:48:35 by abezatog          #+#    #+#             */
-/*   Updated: 2026/08/20 18:39:34 by abezatog         ###   ########.fr       */
+/*   Updated: 2026/08/25 13:53:10 by abezatog         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,14 @@
 
 char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
+	size_t	little_len;
+
 	if (*little == '\0')
 		return ((char *)big);
-	while (len > 0 && *big)
+	little_len = ft_strlen(little);
+	while (len >= little_len && *big)
 	{
-		if (ft_strncmp(big, little, len) == 0)
+		if (ft_strncmp(big, little, little_len) == 0)
 			return ((char *)big);
 		big++;
 		len--;
